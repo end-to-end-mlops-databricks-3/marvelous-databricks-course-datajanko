2612d25e36d09fe8db7e6da0735b4ded86e2a264
datajanko <datajanko>
2025-06-08 20:25:11 +0000

    Compare serving vs models.predict

--- notebooks/week4_feature_lookup_serving.py
+++ notebooks/week4_feature_lookup_serving.py
@@ -71,12 +71,8 @@ schema_name = config.schema_name
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
-overall_feature_table_name = f"{config.catalog_name}.{config.schema_name}.overall_winning_shares_fe_v2"
-per_map_feature_table_name = f"{config.catalog_name}.{config.schema_name}.per_map_winning_shares_fe_v2"
+# overall_feature_table_name = f"{config.catalog_name}.{config.schema_name}.overall_winning_shares_fe_v2"
+# per_map_feature_table_name = f"{config.catalog_name}.{config.schema_name}.per_map_winning_shares_fe_v2"
 
 # COMMAND ----------
 
@@ -85,76 +81,76 @@ workspace = WorkspaceClient()
 
 # COMMAND ----------
 
-overall_spec = OnlineTableSpec(
-            primary_key_columns=["team_name", "date"],
-            source_table_full_name=overall_feature_table_name,
-            run_triggered=OnlineTableSpecTriggeredSchedulingPolicy.from_dict({"triggered": "true"}),
-            perform_full_copy=False,
-            timeseries_key="date"
-        )
-per_map_spec = OnlineTableSpec(
-            primary_key_columns=["map_name", "team_name", "date"],
-            source_table_full_name=per_map_feature_table_name,
-            run_triggered=OnlineTableSpecTriggeredSchedulingPolicy.from_dict({"triggered": "true"}),
-            perform_full_copy=False,
-            timeseries_key="date"
-        )
-workspace.online_tables.create(name=f"{overall_feature_table_name}_online", spec=overall_spec)
-workspace.online_tables.create(name=f"{per_map_feature_table_name}_online", spec=per_map_spec)
+# overall_spec = OnlineTableSpec(
+#             primary_key_columns=["team_name", "date"],
+#             source_table_full_name=overall_feature_table_name,
+#             run_triggered=OnlineTableSpecTriggeredSchedulingPolicy.from_dict({"triggered": "true"}),
+#             perform_full_copy=False,
+#             timeseries_key="date"
+#         )
+# per_map_spec = OnlineTableSpec(
+#             primary_key_columns=["map_name", "team_name", "date"],
+#             source_table_full_name=per_map_feature_table_name,
+#             run_triggered=OnlineTableSpecTriggeredSchedulingPolicy.from_dict({"triggered": "true"}),
+#             perform_full_copy=False,
+#             timeseries_key="date"
+#         )
+# workspace.online_tables.create(name=f"{overall_feature_table_name}_online", spec=overall_spec)
+# workspace.online_tables.create(name=f"{per_map_feature_table_name}_online", spec=per_map_spec)
 
 # COMMAND ----------
 
-# Need to check models.predict first
+#Need to check models.predict first. Doesn't work
 
-# from databricks.sdk import WorkspaceClient
-# from databricks.sdk.service.serving import EndpointCoreConfigInput, ServedEntityInput
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.serving import EndpointCoreConfigInput, ServedEntityInput
 
-# # Create endpoint
-# endpoint_name = "cs-go-feature-serving"
-# endpoint_exists = any(item.name == endpoint_name for item in workspace.serving_endpoints.list())
-# model_name=f"{catalog_name}.{schema_name}.cs_go_model_feature_lookup"
-# model_version=9
-# workspace = WorkspaceClient()
-# served_entities = [
-#     ServedEntityInput(
-#         entity_name=model_name,
-#         scale_to_zero_enabled=True,
-#         workload_size="Small",
-#         entity_version=model_version,
-#         environment_vars={
-#                     "aws_access_key_id": "{{secrets/mlops_course/aws_access_key_id}}",
-#                     "aws_secret_access_key": "{{secrets/mlops_course/aws_secret_access_key}}",
-#                     "region_name": "eu-west-1",
-#                     }
-#     )
-# ]
-# if not endpoint_exists:
-#     workspace.serving_endpoints.create(
-#         name=endpoint_name,
-#         config=EndpointCoreConfigInput(
-#             served_entities=served_entities,
-#         ),
-#     )
-# else:
-#     workspace.serving_endpoints.update_config(
-#         name=endpoint_name,
-#         served_entities=served_entities,
-#     )
+# Create endpoint
+endpoint_name = "cs-go-basoc-feature-serving"
+endpoint_exists = any(item.name == endpoint_name for item in workspace.serving_endpoints.list())
+model_name=f"{catalog_name}.{schema_name}.cs_go_model_basic"
+model_version=9
+workspace = WorkspaceClient()
+served_entities = [
+    ServedEntityInput(
+        entity_name=model_name,
+        scale_to_zero_enabled=True,
+        workload_size="Small",
+        entity_version=model_version,
+        environment_vars={
+                    "aws_access_key_id": "{{secrets/mlops_course/aws_access_key_id}}",
+                    "aws_secret_access_key": "{{secrets/mlops_course/aws_secret_access_key}}",
+                    "region_name": "eu-west-1",
+                    }
+    )
+]
+if not endpoint_exists:
+    workspace.serving_endpoints.create(
+        name=endpoint_name,
+        config=EndpointCoreConfigInput(
+            served_entities=served_entities,
+        ),
+    )
+else:
+    workspace.serving_endpoints.update_config(
+        name=endpoint_name,
+        served_entities=served_entities,
+    )
 
 # COMMAND ----------
 
-# def call_endpoint(record):
-#     """
-#     Calls the model serving endpoint with a given input record.
-#     """
-#     serving_endpoint = f"https://{os.environ['DBR_HOST']}/serving-endpoints/{endpoint_name}/invocations"
+def call_endpoint(record):
+    """
+    Calls the model serving endpoint with a given input record.
+    """
+    serving_endpoint = f"https://{os.environ['DBR_HOST']}/serving-endpoints/{endpoint_name}/invocations"
 
-#     response = requests.post(
-#         serving_endpoint,
-#         headers={"Authorization": f"Bearer {os.environ['DBR_TOKEN']}"},
-#         json={"dataframe_records": record},
-#     )
-#     return response.status_code, response.text
+    response = requests.post(
+        serving_endpoint,
+        headers={"Authorization": f"Bearer {os.environ['DBR_TOKEN']}"},
+        json={"dataframe_records": record},
+    )
+    return response.status_code, response.text
 
 # COMMAND ----------
 
@@ -179,17 +175,25 @@ workspace.online_tables.create(name=f"{per_map_feature_table_name}_online", spec
 
 # COMMAND ----------
 
-# record = [{'date': '2012-03-14',
-#   'team_1': 'Astralis',
-#   'team_2': 'Paradox',
-#   'map_name': 'Mirage',
-#   'starting_ct': 1,
-#   'rank_1': 12,
-#   'rank_2': 10}]
+record = [{'date': '2012-03-14',
+  'team_1': 'Astralis',
+  'team_2': 'Paradox',
+  'map_name': 'Mirage',
+  'starting_ct': 1,
+  'rank_1': 12,
+  'rank_2': 10}]
 
 # COMMAND ----------
 
-# a,b = call_endpoint(record)
+a,b = call_endpoint(record)
+
+# COMMAND ----------
+
+a
+
+# COMMAND ----------
+
+b
 
 # COMMAND ----------
 
