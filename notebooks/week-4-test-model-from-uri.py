f14f6192487146a7cf62fc0df9d1fc709442decb
Jan Koch <jan.koch@tu-dortmund.de>
2025-06-10 22:53:21 +0200

    fix

--- notebooks/week-4-test-model-from-uri.py
+++ notebooks/week-4-test-model-from-uri.py
@@ -12,19 +12,22 @@
 
 # COMMAND ----------
 
+# COMMAND ----------
+import mlflow
 import pandas as pd
 
-# COMMAND ----------
-
-import mlflow
-model_uri = 'runs:/0c35610aa0644a56a106bfdf19b5a8dc/lightgbm-pipeline-model'
-input_data = [{'date': '2012-03-14',
-  'team_1': 'Astralis',
-  'team_2': 'Paradox',
-  'map_name': 'Mirage',
-  'starting_ct': 1,
-  'rank_1': 12,
-  'rank_2': 10}]
+model_uri = "runs:/0c35610aa0644a56a106bfdf19b5a8dc/lightgbm-pipeline-model"
+input_data = [
+    {
+        "date": "2012-03-14",
+        "team_1": "Astralis",
+        "team_2": "Paradox",
+        "map_name": "Mirage",
+        "starting_ct": 1,
+        "rank_1": 12,
+        "rank_2": 10,
+    }
+]
 # Learning: Using the raw list, leads to a strange conversion/schema violation error
 new_data = pd.DataFrame(input_data)
 
@@ -34,5 +37,3 @@ mlflow.models.predict(
 )
 
 # COMMAND ----------
-
-
